39293cb5954d1b57c5aa4a7c87747cc23c0c209c
Moona <aminetou_a.k@hotmail.fr>
2016-09-05 06:08:46 +0200

    suite modif index connexion et inscription

---
+++ vue/contact.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Contact</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/header.css"/>
+</head>
+<body>
+	<h1>Nous contacter</h1>
+	<p>heyhey</p>
+</body>
+</html>
--- vue/header.php
+++ vue/header.php
@@ -4,106 +4,108 @@ if(isset($_SESSION['pseudo']) && $_SESSION['pseudo']!=''){
   $status="Déconnexion";
   $target='deconnexion.php';
 }else{
-  $loginImg='images/cadnas.png';
+  $loginImg='images/lock.png';
   $status="Connexion";
-  $target='connexion.php';
+  $target='connexion2.php';
 }
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-  
-    <link rel="stylesheet" type="text/css" href="css/menu.css"/>
-    <script type="text/javascript" src="script/modernizr.custom.js"></script>
+	<title>Header</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/header.css"/>
 </head>
 
- <body>  
+<body>
 
-   <header id="header">
+	<div id="hautDePage">
 
-    <form action="../controleur/controleur.php" method="POST" align="center" id="search" >
-      <!--  <div id="login"> !-->
-          <p style="text-align: center; ">Recherche</p>
-            <input type="text" name="search" />
-            <img src="images/search.png" id="loupe"/>
-        <!--</div>-!-->
-    </form>  
+		<div id="top">
 
-    <h1><img src='images/titre.jpeg' id="titre"/></h1>
-    <!-- <img src='amotub.png' id="iconeAmotube" /> !-->
-    <div id="droite">
-          <div float="left" style="display: inline;margin-right: 18px"  ><img src='images/english.png' style=' height:15px; with:15px;' /></div>
-          <div style="display: inline ; margin-right: 18px"  float ='left' id ='div2'></div>
-          <div  style="display: inline;margin-right: 18px" float ='left'> ENG</div> 
-          <div float="left" style="display: inline;margin-right: 18px"  ><img src=<?php echo $loginImg;?> style=' height:15px; with:15px;' /></div>
-        
-            <a href=<?php echo $target;?> class="dropbtn" name="connexion"><?php echo $status; ?></a>
-      </div>  
-    </header> 
+		    <form action="../controleur/controleur.php" method="POST" align="center" id="search" >
+		      <!--  <div id="login"> !-->
+		          <p style="text-align: center; ">Recherche
+		            <input type="text" name="search" id='inputSearch'/>
+		            <a href=""> <img src="images/search.png" id="loupe"/></a></p> <!--ECRIRE LIEN POUR RECHERCHE !requetes php-->
+		        <!--</div>-!-->
+	   		</form>  
+	   		
+	   		<img src='images/amotub.png' id="titre"/> 
+	   		
+	   		<div id="droite">
+	   			<img src='images/english.png' class="icones"/>
+	   			<label>ENG</label>
+	   			<img class="icones"/>
 
-    <nav id="menu" class="nav" >  
-     <ul>
+	   			<div float="left" style="display: inline;margin-right: 18px"  ><img src=<?php echo $loginImg;?> style=' height:15px; with:15px;'/></div> <!--demander à cheikh brahim a quoi ça sert -->
 
-      <li>
-        <a href="./index.php"><span  class="icon"> <img aria-hidden="true"  class="ico" src="images/home.png" /></span><span>Accueil</span></a>
-      </li>
+	   			<a href=<?php echo $target;?> class="dropbtn" name="connexion"><?php echo $status; ?></a>
 
-      <li class="dropdown">
-          <a  class="dropbtn" name="leçons" ><span  class="icon"> <img aria-hidden="true"  class="ico"src="images/book.png"/></span><span>Leçons</span></a>
-          <div class="dropdown-content">
-            <a href="#">Math</a>
-            <a href="#">Physique</a>
-            <a href="#">SVT</a>
-          </div>        
-        </li>
+	   		</div>
+		</div>
 
-       <li class="dropdown">
-          <a  class="dropbtn" name="exercies" ><span  class="icon"> <img aria-hidden="true"  class="ico" src="images/pencil2.png"/></span><span>Exercises</span></a>
-          <div class="dropdown-content">
-            <a href="#">Math</a>
-            <a href="#">Physique</a>
-            <a href="#">SVT</a>
-          </div>        
-        </li>
+		<div id="bas" class="nav">
+			<ul>
 
-        <li class="dropdown">
-          <a  class="dropbtn" name="annales" ><span  class="icon"> <img aria-hidden="true"  class="icoAnnales" src="images/books.png"/> </span><span>Annales</span></a>
-          <div class="dropdown-content">
-            <a href="#">Math</a>
-            <a href="#">Physique</a>
-            <a href="#">SVT</a>
-          </div>        
-        </li>
+	      <li>
+	        <a href="./index.php"><span  class="icon"> <img aria-hidden="true"  class="icoAcCon" src="images/home.png" /></span><span>Accueil</span></a>
+	      </li>
 
-        <li class="dropdown">
-          <a  class="dropbtn" name="orientation" ><span  class="icon"> <img aria-hidden="true"  class="ico" src="images/question.png"/></span><span>Orientation</span></a>
-          <div class="dropdown-content">
-            <a href="#">Fiches métier</a>
-            <a href="#">sous menu2</a>
-            <a href="#">sous menu3</a>
-          </div>        
-        </li>
+	      <li class="dropdown">
+	          <a  class="dropbtn" name="leçons" ><span  class="icon"> <img aria-hidden="true"  class="ico"src="images/book.png"/></span><span>Leçons</span></a>
+	          <div class="dropdown-content">
+	            <a href="#">Math</a>
+	            <a href="#">Physique</a>
+	            <a href="#">SVT</a>
+	          </div>        
+	        </li>
 
-         <li class="dropdown">
-          <a  class="dropbtn" name="conseils"> <span  class="icon"> <img aria-hidden="true"  class="ico" src="images/star-empty.png"/></span><span>Conseils et astuces</span> </a>       
-        </li>
+	       <li class="dropdown">
+	          <a  class="dropbtn" name="exercies" ><span  class="icon"> <img aria-hidden="true"  class="ico" src="images/pencil2.png"/></span><span>Exercises</span></a>
+	          <div class="dropdown-content">
+	            <a href="#">Math</a>
+	            <a href="#">Physique</a>
+	            <a href="#">SVT</a>
+	          </div>        
+	        </li>
 
-        <li class="dropdown">
-          <a  class="dropbtn" name="sante"> <span  class="icon"> <img aria-hidden="true"  class="ico"src="images/checkmark.png" /></span><span>Santé</span> </a>      
-        </li>
+	        <li class="dropdown">
+	          <a  class="dropbtn" name="annales" ><span  class="icon"> <img aria-hidden="true"  class="icoAnnales" src="images/books.png"/> </span><span>Annales</span></a>
+	          <div class="dropdown-content">
+	            <a href="#">Math</a>
+	            <a href="#">Physique</a>
+	            <a href="#">SVT</a>
+	          </div>        
+	        </li>
 
-        <li>
-        <a href="index.php"> <span  class="icon"> <img aria-hidden="true"  class="ico" src="images/mail2.png"/></span><span>Nous contacter</span> </a>
-      </li>
+	        <li class="dropdown">
+	          <a  class="dropbtn" name="orientation" ><span  class="icon"> <img aria-hidden="true"  class="ico" src="images/question.png"/></span><span>Orientation</span></a>
+	          <div class="dropdown-content">
+	            <a href="#">Fiches métier</a>
+	            <a href="#">sous menu2</a>
+	            <a href="#">sous menu3</a>
+	          </div>        
+	        </li>
 
-    </ul>
-</nav>
+	         <li class="dropdown">
+	          <a  class="dropbtn" name="conseils" href="conseils.php"> <span  class="icon"> <img aria-hidden="true"  class="ico" src="images/star-empty.png"/></span><span>Conseils et astuces</span> </a>       
+	        </li>
+
+	        <li class="dropdown">
+	          <a  class="dropbtn" name="sante"href="sante.php"> <span  class="icon"> <img aria-hidden="true"  class="ico"src="images/checkmark.png" /></span><span>Santé</span> </a>      
+	        </li>
+
+	        <li>
+	        <a href="index.php"> <span  class="icon" href="contact.php"> <img aria-hidden="true"  class="icoAcCon" src="images/mail2.png"/></span><span>Nous contacter</span> </a>
+	      </li>
+
+	    </ul>
+		</div>
+	</div>
+
+	
+ 	<script type="text/javascript" src="script/modernizr.custom.js"></script>
 </body>
 </html>
-
